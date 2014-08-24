6969102bf9477e76976ac1627fabbe8648a0b5d3
Michael the Great <michaelq.quintero@gmail.com>
2014-08-24 10:49:37 -0400

    Changed character and projectile
    Added animation script

---
+++ Assets/animationScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animationScript : MonoBehaviour {
+	public Sprite [] pictures;
+	int currentPicture;
+	float timer;
+	
+	public void Update()
+	{
+		timer -= Time.deltaTime;
+		if( timer <= 0 )
+		{
+			currentPicture = (currentPicture+1)%pictures.Length;
+			timer = .25F; //1/2 second
+		}
+		//maybe this is inefficient but idgaf
+		gameObject.GetComponent<SpriteRenderer> ().sprite = pictures[currentPicture];
+		
+	}
+	
+}
--- Assets/sceneScroller.cs
+++ Assets/sceneScroller.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class sceneScroller : MonoBehaviour {
 
 	public float speed = 0; //scorll speed
-	
+	//comment to test git settings
 	// Update is called once per frame
 	void Update () {
 		renderer.material.mainTextureOffset = new Vector2(Time.time * speed, 0f);
