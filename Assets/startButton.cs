2689df7ec758c6127d09638a29c43dc15407d3c6
michaelthegreat <michaelq.quintero@gmail.com>
2014-12-18 14:01:59 -0800

    music on/off button

--- Assets/startButton.cs
+++ Assets/startButton.cs
@@ -21,30 +21,62 @@ public class startButton : MonoBehaviour {
 			DontDestroyOnLoad(mManager);
 		}
 	}
+	void Update()
+	{
+		OnGUI ();
+	}
 
 	void OnGUI()
 	{
-		float xPos = Screen.width /2 -10;
+		//button position variables
+		float xPos = Screen.width /2 + 100;
       	float yPos = Screen.height - 70;
-		float optxPos = Screen.width/2 - 250;
+		float optxPos = Screen.width/2 - 300;
 		float optyPos = Screen.height - 70;
-
-	if (GUI.Button ( new Rect (xPos, yPos,200,70), "START"))
+		float musicxPos = Screen.width / 2 - 100;
+		float musicyPos = Screen.height - 70;
+		//Button rectangles
+		Rect musicRect = new Rect (musicxPos, musicyPos, 150, 70);
+	//GUI.BeginGroup (new Rect(0,0,Screen.width,Screen.height));
+	if (GUI.Button ( new Rect (xPos, yPos,150,70), "START"))
 	{
 		Application.LoadLevel ("firstscene");
+	//	GUI.EndGroup ();
 	}
 	
 	string buttonText = "HELP";
 	
-	if(GUI.Button( new Rect(optxPos, optyPos, 200,70),buttonText))
+	if(GUI.Button( new Rect(optxPos, optyPos, 150,70),buttonText))
 	{
 
 
 			Application.LoadLevel("Instructions");
+	//		GUI.EndGroup ();
 	}
+		bool music_enabled = true;
+
+		if((GUI.Button (musicRect, "Toggle Music")))
+		   {
+		
+			if (!GameObject.FindGameObjectWithTag ("Music")) {
+				;
+			}
+			else {
+				if(GameObject.FindGameObjectWithTag ("Music").audio.isPlaying)
+				{
+					GameObject.FindGameObjectWithTag ("Music").audio.Pause ();
+				}
+				else
+				{
+					GameObject.FindGameObjectWithTag ("Music").audio.Play ();
+				}
+
+				//DestroyObject(button);
+				//button = (GUI.Button (new Rect (musicxPos, musicyPos, 150, 70), "Toggle Music"));
+			}
+		}
+
+
 	
-
-
-	GUI.EndGroup ();
 	}
 }
