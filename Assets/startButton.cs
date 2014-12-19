71936d080c6c1aee0877b08f62ac736a87cbbbed
michaelthegreat <michaelq.quintero@gmail.com>
2014-12-19 13:43:53 -0800

    Ads
    An add will display 33% of the time when a user clicks the play button

--- Assets/startButton.cs
+++ Assets/startButton.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class startButton : MonoBehaviour {
 
+
+
 	void Awake() { 
 
 
@@ -14,12 +16,22 @@ public class startButton : MonoBehaviour {
     public Transform musicPrefab;
     void Start()
 	{
+
+		//If we havent made the music manager yet then make it
+		//And don't destroy it when we switch scenes
 		if(!GameObject.FindGameObjectWithTag("Music"))
 		{
 			var mManager = Instantiate(musicPrefab, transform.position, Quaternion.identity) as Transform; 
 			mManager.name = musicPrefab.name;
 			DontDestroyOnLoad(mManager);
 		}
+		//Ad scripts and shit
+		AdBuddizBinding.SetLogLevel(AdBuddizBinding.ABLogLevel.Info);         // log level
+		AdBuddizBinding.SetAndroidPublisherKey("TEST_PUBLISHER_KEY_ANDROID"); // replace with your Android app publisher key
+		AdBuddizBinding.SetIOSPublisherKey("TEST_PUBLISHER_KEY_IOS");         // replace with your iOS app publisher key
+		AdBuddizBinding.SetTestModeActive();                                  // to delete before submitting to store
+		AdBuddizBinding.CacheAds();  
+
 	}
 	void Update()
 	{
@@ -37,11 +49,19 @@ public class startButton : MonoBehaviour {
 		float musicyPos = Screen.height - 70;
 		//Button rectangles
 		Rect musicRect = new Rect (musicxPos, musicyPos, 150, 70);
+
+    //Buttons
 	//GUI.BeginGroup (new Rect(0,0,Screen.width,Screen.height));
 	if (GUI.Button ( new Rect (xPos, yPos,150,70), "START"))
 	{
-		Application.LoadLevel ("firstscene");
-	//	GUI.EndGroup ();
+		Random.seed = (int)System.DateTime.Now.Ticks;
+		if (Random.Range (1, 3) == 2) 
+		{
+			AdBuddizBinding.ShowAd();
+		}
+		else
+		  Application.LoadLevel ("firstscene");
+		//	GUI.EndGroup ();
 	}
 	
 	string buttonText = "HELP";
@@ -53,7 +73,7 @@ public class startButton : MonoBehaviour {
 			Application.LoadLevel("Instructions");
 	//		GUI.EndGroup ();
 	}
-		bool music_enabled = true;
+	
 
 		if((GUI.Button (musicRect, "Toggle Music")))
 		   {
@@ -79,4 +99,56 @@ public class startButton : MonoBehaviour {
 
 	
 	}
+
+	void OnEnable()
+	{
+		// Listen to AdBuddiz events
+		AdBuddizManager.didFailToShowAd += DidFailToShowAd;
+		AdBuddizManager.didCacheAd += DidCacheAd;
+		AdBuddizManager.didShowAd += DidShowAd;
+		AdBuddizManager.didClick += DidClick;
+		AdBuddizManager.didHideAd += DidHideAd;
+	}
+	
+	void OnDisable()
+	{
+		// Remove all event handlers
+		AdBuddizManager.didFailToShowAd -= DidFailToShowAd;
+		AdBuddizManager.didCacheAd -= DidCacheAd;
+		AdBuddizManager.didShowAd -= DidShowAd;
+		AdBuddizManager.didClick -= DidClick;
+		AdBuddizManager.didHideAd -= DidHideAd;
+
+	}
+	
+	void DidFailToShowAd(string adBuddizError) {
+		AdBuddizBinding.LogNative("DidFailToShowAd: " + adBuddizError);
+		Debug.Log ("Unity: DidFailToShowAd: " + adBuddizError);
+	}
+	
+	void DidCacheAd() {
+		AdBuddizBinding.LogNative("DidCacheAd");
+		Debug.Log ("Unity: DidCacheAd");
+
+	}
+	
+	void DidShowAd() {
+		AdBuddizBinding.LogNative("DidShowAd");
+		Debug.Log ("Unity: DidShowAd");
+	}
+	
+	void DidClick() {
+		AdBuddizBinding.LogNative("DidClick");
+		Debug.Log ("Unity: DidClick");
+	}
+	
+	void DidHideAd() {
+		AdBuddizBinding.LogNative("DidHideAd");
+		Debug.Log ("Unity: DidHideAd");
+		Application.LoadLevelAsync ("firstscene");
+
+	}
+	/*
+}*/
+
 }
